7b82319e0972d3e44fc3908e05b1ac098d0dd623
Kangnos <57825621+Kangnos@users.noreply.github.com>
2020-09-10 17:50:04 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from bs4 import BeautifulSoup
 import requests
 
-r = requests.get("https://www.youtube.com/watch?v=lqYQXIt4SpA")
-soup = BeautifulSoup(r.text, "html.parser")
-artist = soup.select('a[href="/channel/UCJrOtniJ0-NWz37R30urifQ"]')
-print(artist)
+url = requests.get("https://www.youtube.com/watch?v=rcaz3Ai7Ouk")
+soup = BeautifulSoup(url.text, "html.parser")
+
+url = soup.find("title")
+print(url.get_text())
