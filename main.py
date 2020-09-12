e5ca1f912f2f9b415cadea3398062493916098b1
Kangnos <57825621+Kangnos@users.noreply.github.com>
2020-09-12 23:30:34 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 
 url = requests.get("https://www.youtube.com/watch?v=rcaz3Ai7Ouk")
-soup = BeautifulSoup(url.text, "html.parser")
+soup = BeautifulSoup(url, "html.parser")
 
-url = soup.find("title")
-print(url.get_text())
+url = soup.select("h1.title style-scope ytd-video-primary-info-renderer")
+print(url)
